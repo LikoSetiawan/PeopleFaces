b1e2ad9b3ed107665e9d0c0467b11fcf18833f4e
LikoSetiawan <128354782+LikoSetiawan@users.noreply.github.com>
2024-04-17 22:44:48 +0800

    Initial commit

---
+++ ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  PeopleFaces
+//
+//  Created by Liko Setiawan on 17/04/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ PeopleFacesApp.swift
@@ -0,0 +1,17 @@
+//
+//  PeopleFacesApp.swift
+//  PeopleFaces
+//
+//  Created by Liko Setiawan on 17/04/24.
+//
+
+import SwiftUI
+
+@main
+struct PeopleFacesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
